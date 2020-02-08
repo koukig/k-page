e9edbd162e77ae464afb95c7534f449b765f5944
koukig <okkoukig@gmail.com>
2020-02-08 11:06:59 +0900

    会社用ホームページ　ベース作成

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,12 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { RouterModule, Routes } from "@angular/router";
 
+import { NgModule } from "@angular/core";
+import { TopPageComponent } from "./top-page/top-page.component";
 
-const routes: Routes = [];
+const routes: Routes = [{ path: "", component: TopPageComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,21 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppComponent } from "./app.component";
+import { AppRoutingModule } from "./app-routing.module";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { BrowserModule } from "@angular/platform-browser";
+import { MainHeaderComponent } from "./main-header/main-header.component";
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { NgModule } from "@angular/core";
+import { TopPageComponent } from "./top-page/top-page.component";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, TopPageComponent, MainHeaderComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
